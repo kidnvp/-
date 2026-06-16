1885c4d68f06164f794d69c114298cf3bfb008a9
kidnvp <feltsickaf@gmail.com>
2026-06-16 20:42:11 +0700

    bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -24,9 +24,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "Made with ❤️",
+        name: "กุเป็นทาส",
         // Activity type number (0 = Playing).
-        type: 0,
+        type: 4,
       },
     ],
   },
